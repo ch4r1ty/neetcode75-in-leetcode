29543c4944fa9a4262c058dd901f1dcea53bfea5
peter <peteryuan2024@gmail.com>
2024-12-27 17:05:06 -0500

    121

---
+++ Sliding Window/121. Best Time to Buy and Sell Stock/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+
+        for (int price : prices) {
+            maxProfit = Math.max(maxProfit, price - minPrice);
+            minPrice = Math.min(minPrice, price);
+        }
+
+        return maxProfit;
+    }
+}
